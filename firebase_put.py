04ad2d5dc4b37969ad21905f2fccd4c90416c12e
jb <46815338+officialtech@users.noreply.github.com>
2019-03-07 12:26:57 +0530

    Update and rename firebase1.py to firebase_put.py

--- firebase_put.py
+++ firebase_put.py
@@ -1,5 +1,5 @@
 from firebase.firebase import FirebaseApplication # calling one Class only
-fire = FirebaseApplication("https://officialtech-team.firebaseio.com/")
+fire = FirebaseApplication("url")
 dictonary = {"merchant":{"employee":{"admin":{"101":{"username": "rooh", "password": "rp808"},
                                                "102":{"username":"off944", "password": "tech944"}},"stocker":{
         "1001":{"username":"techque", "password":"techno123"},"1002":{"username":"ac", "password": "kc123"}
